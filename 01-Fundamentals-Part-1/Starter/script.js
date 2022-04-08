0ad57e073e205bb2552abe6302461178b78dc577
gupta_keshav_001378461 <gupta.k@husky.neu.edu>
2022-04-08 18:41:29 -0400

    Completed Fundamental part 1
    Completed Fundamental part 1

---
+++ 01-Fundamentals-Part-1/Starter/script.js
@@ -0,0 +1,25 @@
+/*let js = 'amazing';
+console.log(40 + 8 + 23 - 10);
+
+console.log("Keshav");
+console.log(26);
+
+let firstName = "Keshav";
+console.log(firstName);
+console.log(firstName);
+console.log(firstName);
+
+let age = 30;
+age = 31;
+
+const birthyear = 1995;
+birthyear = 1996;
+*/
+
+const now = 2037;
+
+const ageKeshav = now - 1995;
+const ageAnjali = now - 1998;
+console.log(ageAnjali, ageKeshav);
+
+console.log(ageKeshav * 2, ageKeshav / 10, 2 ** 3);
